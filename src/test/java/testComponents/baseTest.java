8829143ffce4d382d6b08cbdf4e10e0c2e271868
ibrahim3rafa <ibrahimarafa611@gmail,com>
2025-04-22 20:10:02 +0200

    Run in headless mode

--- src/test/java/testComponents/baseTest.java
+++ src/test/java/testComponents/baseTest.java
@@ -2,11 +2,16 @@ package testComponents;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import pageObjects.landingPage;
@@ -30,14 +35,23 @@ public class baseTest {
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//main//resources//globalData.properties");
         prop.load(fis);
-        String browserName = prop.getProperty("browser");
+        String browserName =  System.getProperty("browser") != null? System.getProperty("browser"):prop.getProperty("browser");
+      //  String browserName = prop.getProperty("browser");
 
-        if (browserName.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+        if (browserName.contains("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            if (browserName.contains("headless")) {
+                options.addArguments("headless");
+            }
+            driver = new ChromeDriver(options);
+            driver.manage().window().setSize(new Dimension(1440,990));
         } else if (browserName.equalsIgnoreCase("firefox")) {
             //firefox
+          //  WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
         } else if (browserName.equalsIgnoreCase("edge")) {
             //edge
+            driver = new EdgeDriver();
         }
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
